dad2a4815e27ef1d369a136ff1407b343ead5fd9
Sergio Moreira <sergioricardomoreirasilva@gmail.com>
2022-09-21 15:11:31 -0300

    Desenvolvimento do Requisito 02

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     // return (<p>Sistema Solar</p>);
     return (
-      <Header />
+      <div>
+        <Header />
+        <SolarSystem />
+      </div>
     );
   }
 }

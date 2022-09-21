da7f8bed687a13b9918cb4e47882e72b8afb6c68
Sergio Moreira <sergioricardomoreirasilva@gmail.com>
2022-09-21 18:58:16 -0300

    Desenvolvimento do Requisito 07

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
+        <Missions />
       </div>
     );
   }

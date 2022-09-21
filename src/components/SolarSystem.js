b963789f449750c20a6ed243f8391d9507bf7915
Sergio Moreira <sergioricardomoreirasilva@gmail.com>
2022-09-21 17:14:40 -0300

    Desenvolvimento do Requisito 04

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Title from './Title'; // Inicialmente eu tentei importar para o App.js mas o requisito pede para importar para o Componente SolarSystem.js;
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system" />
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
     );
   }
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -19,5 +19,5 @@ export default Title;
 /*
 1° Tentativa: <h2>{ this.props.headline }</h2>, mas o lint acusou "Must use destructuring props assignment". Poderia ser resolvido excluindo o .headline, mas optei pela maneira como foi feita. Site de apoio: https://reactjs.org/docs/components-and-props.html
 
-Inicialmente eu tinha importado o Title para o App, mas não é um requisito da questão então eu esclui a importação.
+Inicialmente eu tinha importado o Title para o App, mas não é um requisito da questão então eu exclui a importação.
 */

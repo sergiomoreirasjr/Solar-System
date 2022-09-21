f5b4abf150a60ce424460f95e4441721c1755be5
Sergio Moreira <sergioricardomoreirasilva@gmail.com>
2022-09-21 20:26:40 -0300

    Desenvolvimento do Requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <section>
+          { missions.map((missão) => (
+            <MissionCard
+              key={ missão.name } // O name é único para cada missão, então pode ser utilizado como parâmetro. Todos que forem únicos podem.
+              name={ missão.name }
+              year={ missão.year }
+              country={ missão.country }
+              destination={ missão.destination }
+            />
+          ))}
+        </section>
       </div>
     );
   }
 }
 
 export default Missions;
+
+// Para  realização desse requisito, eu usei o arquivo SolarSystem.js realizado anteriormente (Req. 06);

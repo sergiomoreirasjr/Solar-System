b97ce22cec269bb77908de2a11febb1afb1fd854
Sergio Moreira <sergioricardomoreirasilva@gmail.com>
2022-09-21 19:56:33 -0300

    Desenvolvimento do Requisito 09

---
+++ src/components/MissionCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MissionsCard extends React.Component {
+  render() {
+    const { name } = this.props;
+    const { year } = this.props;
+    const { country } = this.props;
+    const { destination } = this.props;
+
+    return (
+      <div data-testid="mission-card">
+        <p data-testid="mission-name">{ name }</p>
+        <p data-testid="mission-year">{ year }</p>
+        <p data-testid="mission-country">{ country }</p>
+        <p data-testid="mission-destination">{ destination }</p>
+      </div>
+    );
+  }
+}
+
+MissionsCard.propTypes = {
+  name: PropTypes.string,
+  year: PropTypes.string,
+  country: PropTypes.string,
+  destination: PropTypes.string,
+}.isRequired;
+
+export default MissionsCard;
+
+// Para a realização desse requisito, eu usei o arquivo PlanetCard.js realizado anteriormente (Req. 05);

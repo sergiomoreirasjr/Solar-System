745310ed6c55fbe746f6c58573b4b6e649bf1713
Sergio Moreira <sergioricardomoreirasilva@gmail.com>
2022-09-21 17:57:16 -0300

    Desenvolvimento do Requisito 05

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName } = this.props;
+    const { planetImage } = this.props;
+
+    return (
+      <div data-testid="planet-card">
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{ planetName }</p>
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string,
+  planetImage: PropTypes.string,
+}.isRequired;
+
+export default PlanetCard;
+
+// Na linha 11, o Template Literals foi utilizado porque além do valor recebido por planetName} eu preciso da string "Planeta".
+// Site que consultei para tirar dúvidas: https://stackoverflow.com/questions/49735097/template-literals-in-jsx
